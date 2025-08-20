0f4f1532db0caa7f44e297b501587d018ebdd559
P0lonia <gazunano@gmail.com>
2025-08-19 21:03:37 -0300

    componetizando a aplicação

---
+++ src/components/Tarefas.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const props = defineProps(['tarefas', 'tarefasB'])
+</script>
+
+<template>
+    <ul v-if="tarefasB > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizado = evento.target.checked" :checked="tarefa.finalizado"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizado }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <h3 class="mt-5 d-flex justify-content-center" v-else>Não há tarefas...</h3>
+</template>
